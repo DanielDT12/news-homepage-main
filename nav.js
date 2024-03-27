0526b1d868a00d844ca6b743e25ea8e6e5858fa9
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-03-27 16:21:45 +0100

    startet å lage mobil meny

---
+++ nav.js
@@ -0,0 +1,6 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    
+})
